6822ee430e062ba6f9cd03325b39f7922bc61c01
sarthakj1959 <91320181+sarthakj1959@users.noreply.github.com>
2021-09-24 15:59:37 +0530

    Add files via upload

---
+++ buzzer.ino
@@ -0,0 +1,16 @@
+void setup() {
+ pinMode(9,OUTPUT);
+ pinMode(10,INPUT);
+
+}
+
+void loop() {
+{
+  if(digitalRead(10)==1)
+digitalWrite(9,1);
+}
+  {
+    if(digitalRead(10)==0)
+  digitalWrite(9,0);
+  }
+}
